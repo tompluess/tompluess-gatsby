626954d7b7b933f260f9b2b6630b1d9022f8cfcf
Tom Plüss <tom.pluess@simplificator.com>
2022-05-17 19:33:21 +0200

    Add gatsby-plugin-manifest, some dependeny updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,18 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
     `gatsby-plugin-offline`,
     `gatsby-transformer-json`,
     `gatsby-transformer-remark`,
